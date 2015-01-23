bd5bbe61a68bdf3a81a69c2a18ccb03380db9ef5
Reedyseth <reedyseth@gmail.com>
2015-01-22 17:14:29 -0700

    Change constructore to allow parameters to configure the interaction with the database with TNS Names and Safe Transactions. Add Safe Transactions logic on the insert_record function. Change Code Styling format using the Standar from WordPress. Add more Throwing Exceptions to allow the developer to catch them.

--- kokoroForOracle.php
+++ kokoroForOracle.php
@@ -1,58 +1,147 @@
 <?php
+
 /**
  *
  *
  * @author  Israel Barragan C.
  * @version 150112
  * @since   12-Jan-2015
- * @name kokoroForOracle.php
+ * @name kokoroForOracle .php
  */
 class kokoroForOracle extends kokoroDB implements IkokoroDB {
-	private $conn          = null;
-	private $connection    = null;
-	private $dt            = null;
-	private $data          = null;
-	private $errorMessage  = array();
+	private $conn             = null;
+	private $connection       = null;
+	private $dt               = null;
+	private $data             = null;
+	private $errorMessage     = array();
+	private $useTNSName       = false;
+	private $safeTransactions = null;
 
-	function __construct() {
+	/**
+	 * The constructor will set the TNS Name usage and the Safe Transaction.
+	 *
+	 * @param null $useTNSName Optional parameter, if not parameter is provided then the connection uses the connection
+	 *                         settings. If the the parameter is define the connection will be perform using the TNS
+	 *                         Name. This TNS Name has to be define on your tnsnames.ORA file.
+	 * @param bool $safeTxn
+	 */
+	function __construct( $useTNSName = null, $safeTxn = false ) {
+		if ( $useTNSName != null ) {
+			$this->setUseTNSName( $useTNSName );
+		}
+		if ( $safeTxn == true ) {
+			$this->setSafeTransactions( true );
+		} else {
+			$this->setSafeTransactions( false );
+		}
 		$this->create_connection();
 	}
 
 	function create_connection() {
 		try {
-            // Oracle Connection.
-            $conn = new PDO('oci:dbname='.$this->getDB_host().':'.$this->getDB_port().'/'.$this->getDB_name(),
-            		$this->getDB_user(),$this->getDB_pwd() );
-            // Lets make sure that PDO will throw exceptions.
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			// Since working with Oracle is a bit different, we have to set the explicit date format
-			$pstmt = $conn->prepare("ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS'" );
-			$pstmt->execute();
-            // Now we set the connection attribute.
-			$this->setConnection($conn);
-		} catch(PDOException $ex) {
-			$errorMessage["ex_db_connection_error"] = $ex;
-            $errorMessage["message_db_connection_error"] = "Cannot connect to database";
-            $this->setErrorMessage($errorMessage);
-            throw new Exception($ex, 1111);
-        }
-	}
-	// 15-Jul-2014: New method to insert a record
-	public function insert_record($sql, $data = null){
-		$this->conn = $this->getConnection();
-		try {
-			$this->dt = $this->conn->prepare($sql);
-			$this->dt->execute( $data );
-		} catch (PDOException $ex) {
-			if ( $ex->getCode() == "HY093" ) {
-				$errorMessage["message_error"] = $ex->getMessage();
+			if ( $this->getUseTNSName() ) {
+				// Oracle Connection with TNS Name.
+				$conn = new PDO( 'oci:dbname=' . $this->getDB_sid(),
+					$this->getDB_user(), $this->getDB_pwd() );
+			} else {
+				// Oracle Connection with no TNS Name.
+				$conn = new PDO( 'oci:dbname=' . $this->getDB_host() . ':' . $this->getDB_port() . '/' . $this->getDB_name(),
+					$this->getDB_user(), $this->getDB_pwd() );
 			}
-			$errorMessage["ex_insert_error"] = $ex->getMessage();
-			$this->setErrorMessage( $errorMessage );
-			$this->dt->closeCursor();
-
-			return false;
+			// Oracle Connection.
+			// Lets make sure that PDO will throw exceptions.
+			$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			// Since working with Oracle is a bit different, we have to set the explicit date format
+			$pstmt = $conn->prepare( "ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS'" );
+			$pstmt->execute();
+			// Now we set the connection attribute.
+			$this->setConnection( $conn );
 		}
+		catch ( PDOException $ex ) {
+			$errorMessage["ex_db_connection_error"]      = $ex;
+			$errorMessage["message_db_connection_error"] = "Cannot connect to database";
+			$this->setErrorMessage( $errorMessage );
+			throw new Exception( $ex, 1111 );
+		}
+	}
+
+	/**
+	 * Check if there is an active transaction on the driver, if there is not an active transaction
+	 * it creates a new one.
+	 * @since 22-Jan-2015
+	 */
+	public function beginTransaction() {
+		$this->conn = $this->getConnection();
+		if ( ! $this->conn->inTransaction() ) {
+			$this->conn->beginTransaction();
+		}
+	}
+
+	/**
+	 * Commit the current transaction
+	 * @since 22-Jan-2015
+	 */
+	public function commit() {
+		$this->conn = $this->getConnection();
+		$this->conn->commit();
+	}
+
+	/**
+	 * Rollback the current transaction
+	 * @since 22-Jan-2015
+	 */
+	public function rollBack() {
+		$this->conn = $this->getConnection();
+		$this->conn->rollBack();
+	}
+
+	/**
+	 * Insert a Record on the database. If $safeTransactions is enable then it will work With Safety Transaction.
+	 *
+	 * @param      $sql
+	 * @param null $data
+	 *
+	 * @since 15-Jul-2014
+	 *
+	 * @return bool
+	 * @throws Exception
+	 */
+	public function insert_record( $sql, $data = null ) {
+		$this->conn = $this->getConnection();
+
+		if ( $this->getSafeTransactions() ) {
+			try {
+				$this->beginTransaction();
+				$this->dt = $this->conn->prepare( $sql );
+				$this->dt->execute( $data );
+				$this->commit();
+
+			}
+			catch ( PDOException $ex ) {
+				$errorMessage["ex_insert_error"] = $ex->getMessage();
+				$this->setErrorMessage( $errorMessage );
+				$this->dt->closeCursor();
+				$this->rollBack();
+				throw new Exception( "There was a problem inserting the record." );
+
+				return false;
+			}
+		} else if ( ! $this->getSafeTransactions() ) {
+			try {
+				$this->dt = $this->conn->prepare( $sql );
+				$this->dt->execute( $data );
+			}
+			catch ( PDOException $ex ) {
+				$errorMessage["ex_insert_error"] = $ex->getMessage();
+				$this->setErrorMessage( $errorMessage );
+				$this->dt->closeCursor();
+				throw new Exception( "There was a problem inserting the record." );
+
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	public function query_all_data( $sql, $data = null ) {
@@ -70,6 +159,7 @@ class kokoroForOracle extends kokoroDB implements IkokoroDB {
 			$errorMessage["ex_query_error"] = $ex->getMessage();
 			$this->setErrorMessage( $errorMessage );
 			$this->dt->closeCursor();
+			throw new Exception( $ex->getMessage() );
 
 			return false;
 		}
@@ -223,15 +313,51 @@ class kokoroForOracle extends kokoroDB implements IkokoroDB {
 	}
 
 
-    /**
-     * Gets the value of driverOptions.
-     *
-     * @return mixed
-     */
-    public function getDriverOptions()
-    {
-        return $this->driverOptions;
-    }
+	/**
+	 * Gets the value of driverOptions.
+	 *
+	 * @return mixed
+	 */
+	public function getDriverOptions() {
+		return $this->driverOptions;
+	}
+
+	/**
+	 * Gets the value of useTNSName    .
+	 *
+	 * @return mixed
+	 */
+	public function getUseTNSName() {
+		return $this->useTNSName;
+	}
+
+	/**
+	 * Sets the value of useTNSName    .
+	 *
+	 * @param mixed $useTNSName the useTNSName
+	 *
+	 * @return self
+	 */
+	public function setUseTNSName( $useTNSName ) {
+		$this->useTNSName = $useTNSName;
+
+		return $this;
+	}
+
+	/**
+	 * @param null $safeTransactions
+	 */
+	public function setSafeTransactions( $safeTransactions ) {
+		$this->safeTransactions = $safeTransactions;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function getSafeTransactions() {
+		return $this->safeTransactions;
+	}
+
 }
 
 ?>
